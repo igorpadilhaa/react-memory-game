9ef18824c94810207637eeba3792ae213241e822
padilha <igor.padilha2004@gmail.com>
2023-06-12 20:25:16 -0300

    create main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
+import GameBoard from "./components/GameBoard"
+
+import coffeeImage from './assets/coffee.png'
+
+import style from './App.module.css'
+
 function App() {
   return (
-    <h1>hello from a memory game! :D</h1>
+    <main className={style.mainContent}>
+      <h1 className={style.title}>Wow a memory game! :D</h1>
+      <section className="board">
+        <GameBoard size="large" tiles={Array(24).fill({ cardImage: coffeeImage })} />
+      </section>
+    </main>
   )
 }
 
