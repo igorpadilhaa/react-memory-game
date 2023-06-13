9552bfe678403f044e0bbbf8574e4f31df71fc14
padilha <igor.padilha2004@gmail.com>
2023-06-12 22:28:43 -0300

    remove unused props from CardTile component

--- src/components/CardTile/types.tsx
+++ src/components/CardTile/types.tsx
@@ -2,8 +2,6 @@ import { MouseEventHandler } from "react"
 
 interface CardTileProps {
     cardImage: string,
-    backgroundColor?: string,
-    borderColor?: string,
     show?: boolean,
     onClick?: MouseEventHandler
 }
