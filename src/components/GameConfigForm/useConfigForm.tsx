d7fd50b0273cfca40700c75f0897f01310f532e9
padilha <igor.padilha2004@gmail.com>
2023-06-18 15:57:48 -0300

    fix useConfigForm to avoid rerendering

--- src/components/GameConfigForm/useConfigForm.tsx
+++ src/components/GameConfigForm/useConfigForm.tsx
@@ -1,17 +1,17 @@
-import { useRef } from "react";
+import { useCallback, useRef } from "react";
 import { GameConfigForm } from ".";
 import { GameConfigFormProps } from "./types";
 
 function useConfigForm(props?: GameConfigFormProps) {
     const formRef = useRef<HTMLDialogElement>(null)
 
-    const Form = () => {
+    const Form = useCallback(() => {
         return <GameConfigForm ref={formRef} {...props}/>;
-    }
+    }, [formRef, props])
 
-    const show = () => {
+    const show = useCallback(() => {
         formRef.current && !formRef.current.open && formRef.current.showModal()
-    }
+    }, [formRef])
 
     return {
         show,
