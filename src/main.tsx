54c05107ed64a020052a93d19361b8aab288e875
padilha <igor.padilha2004@gmail.com>
2023-06-18 16:54:06 -0300

    create About page

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { RouterProvider, createHashRouter } from 'react-router-dom'
+import About from './About.tsx'
+
+const router = createHashRouter([
+  {
+    path: '/',
+    element: <App />
+  },
+  {
+    path: '/about',
+    element: <About />
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
